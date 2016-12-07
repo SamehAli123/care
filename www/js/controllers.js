0b82f6e9d986a6c7eac538b11de689ce5ed48972
Joker <Joker>
2016-12-07 23:29:37 +0300

    test

---
+++ www/js/controllers.js
@@ -0,0 +1,2 @@
+var appControllers = angular.module('starter.controllers', []); // Use for all controller of application.
+var appServices = angular.module('starter.sevices', []);// Use for all service of application.
---
+++ www/templates/material-user-interface/dashboard/js/controllers.js
@@ -0,0 +1,41 @@
+// Controller of dashboard.
+appControllers.controller('dashboardCtrl', function ($scope, $timeout, $state,$stateParams, $ionicHistory) {
+  
+    //$scope.isAnimated is the variable that use for receive object data from state params.
+    //For enable/disable row animation.
+    $scope.isAnimated =  $stateParams.isAnimated;
+
+    // navigateTo is for navigate to other page 
+    // by using targetPage to be the destination state. 
+    // Parameter :  
+    // stateNames = target state to go.
+    $scope.navigateTo = function (stateName) {
+        $timeout(function () {
+            if ($ionicHistory.currentStateName() != stateName) {
+                $ionicHistory.nextViewOptions({
+                    disableAnimate: false,
+                    disableBack: true
+                });
+                $state.go(stateName);
+            }
+        }, ($scope.isAnimated  ? 300 : 0));
+    }; // End of navigateTo.
+
+    // goToSetting is for navigate to Dashboard Setting page
+   
+
+
+    ///////////// for menu conroller
+
+
+
+
+
+
+
+
+
+
+}); // End of dashboard controller.
+
+// Controller of Dashboard Setting.
